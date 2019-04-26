feef3e8fc54a18dd7e60beaa8fc25cbc15fd3c10
KnightZhang <knight.625@live.com>
2019-04-26 13:01:15 +0800

    finish rnn lm

--- Tutorial/tensorflow/1_rnn_lm/model.py
+++ Tutorial/tensorflow/1_rnn_lm/model.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tensorflow as tf
 
+from batch_the_data import read_data, make_batches
+
 ############################ define hyper-parameters ############################ 
 TRAIN_DATA = 'ptb.train'
 EVAL_DATA = 'ptb.valid'
@@ -42,8 +44,10 @@ class Model(object):
                                 for _ in range(NUM_LAYERS)]
         cell = tf.nn.rnn_cell.MultiRNNCell(lstm_cells)
 
+        ########################### graph ###########################
         # initial_state contains (h, c)
         self.initial_state = cell.zero_state(batch_size, tf.float32)
+        #############################################################
 
         # define the embedding layer
         # if not initializer passed, the defalut initializer passed in the variable scope will be used,
@@ -78,8 +82,11 @@ class Model(object):
                 labels = tf.reshape(self.targets, [-1]),
                 logits = logits
         )
+
+        ########################### graph ###########################
         self.cost = tf.reduce_sum(loss) / batch_size
         self.final_state = state
+        ############################################################
 
         if not is_training : return
 
@@ -87,6 +94,55 @@ class Model(object):
         grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, train_variables), MAX_GRAD_NORM)
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=1.0)
         self.train_op = optimizer.apply_gradients(zip(grads, train_variables))
+
+def run_epoch(session, model, batches, train_op, output_log, step):
+    total_cost = 0.0
+    iters = 0
+    state = session.run(model.initial_state)
+
+    for x, y in batches:    # x -> (batch_size, num_steps)
+        cost, state, _ = session.run([model.cost, model.final_state, train_op], 
+                                     {model.input_data : x, model.targets : y, model.initial_state : state})
+
+        total_cost += cost
+        iters += model.num_steps
+
+        if output_log and step % 100 == 0:
+            print('After %d steps, perplexity is %.3f'%(step, np.exp(total_cost / iters)))
+        
+        step += 1
+    
+    return step, np.exp(total_cost / iters)
+
+def main():
+    initializer = tf.random_uniform_initialzer(-0.05, 0.05)
+
+    with tf.variable_scope('language_model', reuse=None, initializer=initializer):
+        train_model = Model(True, TRAIN_BATCH_SIZE, TRAIN_NUM_STEP)
+    
+    with tf.variable_scope('language_model', reuse=True, initializer=initializer):
+        eval_model = Model(False, EVAL_BATCH_SIZE, EVAL_NUM_STEP)
+    
+    with tf.Session() as session:
+        tf.global_variable_initializer().run()
+
+        train_batches = make_batches(read_data(TRAIN_DATA), TRAIN_BATCH_SIZE, TRAIN_NUM_STEP)
+        eval_batches = make_batches(read_data(EVAL_DATA), EVAL_BATCH_SIZE, EVAL_NUM_STEP)
+        test_batches = make_batches(read_data(TEST_DATA), EVAL_BATCH_SIZE, EVAL_NUM_STEP)
+        
+        step = 0
+
+        for i in range(NUM_EPOCH):
+            print('In iteration : %d'%(i+1))
+            step, train_pplx = run_epoch(session, train_model, train_batches, train_model.train_op, True, step)
+            print('STEP : %d'%(step))
+            print('Epoch %d Train Perplexity : %.3f'%(i+1, train_pplx))
+
+            _, eval_pplx = run_epoch(session, eval_model, eval_batches, tf.no_op(), False, 0)
+            print('Epoch : %d Eval Perplexity : %.3f'%(i+1, eval_pplx))
+        
+        _, test_pplx = run_epoch(session, eval_model, test_batches, tf.np_op(), False, 0)
+        print('Test Perplexity %.3f'%(test_pplx))
 
 if __name__ == '__main__':
     model = Model(True, TRAIN_BATCH_SIZE, TRAIN_NUM_STEP)
